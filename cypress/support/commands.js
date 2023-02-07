0e0541c49efe6cee5e36837716bc4838ac7df763
MichelHeydt <michel.heydt@gft.com>
2023-02-07 09:52:05 -0300

    remove comment in file commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,6 +2,6 @@ Cypress.Commands.add('fillMandatoryFieldsAndSubmit', () => {
     cy.get('#firstName').type('Michel')
     cy.get('#lastName').type('Heydt')
     cy.get('#email').type('michel@cvc.com.br')
-    cy.get('#open-text-area').type('Teste') //delay = 0 para digitar um texto longo imediatamente.
+    cy.get('#open-text-area').type('Teste') 
     cy.contains('button', 'Enviar').click()
 })
